c76bac4d87a482bcd53e584c321bd4c8402e1eb4
Hemant Torsekar <hvt@me.com>
2018-10-16 19:06:29 -0400

    Setup for load thirty articles

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,15 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 	var btn = document.getElementById('more-posts');
-	var bio = document.getElementById('recent-articles');
+	var ul = document.getElementById('recent-articles');
 
-
-	btn.addEventListener('onclick', function(event) {
+	var next_article = document.getElementById('next-article');
+	
+	btn.addEventListener('click', function(event) {
 		Rails.ajax({
 			url: 'articles/load_articles',
 			type: "GET",
-			success: function(event) {
-				event.preventDefault();
+			data: "next_article=" + next_article.value,
+
+			success: function(document) {  
+				# ul.innerHTML = document.documentElement.innerHTML;
+				# alert(document.documentElement.innerHTML);
+			}, 
+			error: function () {
+				console.log("error");
 			}
 		});
 	});
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,15 @@
 class ArticlesController < ApplicationController
+  @@page_size = 5
   def index
   	@articles = ArticleCache.retrieve(30,0)
+    @next_article = @@page_size
   end
 
   def load_articles
     # TODO: remember strong params
-    @articles = ArticleCache.retrieve(30, params["n"])
+    @next_article = params["next-article"] || 0
+    @articles = ArticleCache.retrieve(30, @next_article)
+    @next_article += @@page_size
+    render html: "articles"
   end
 end
