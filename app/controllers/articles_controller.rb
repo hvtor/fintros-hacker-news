415b0db72c99a59ab87da748978fc41b209e6881
Hemant Torsekar <hvt@me.com>
2018-10-21 19:45:21 -0400

    Load more posts works.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,15 @@
 class ArticlesController < ApplicationController
-  @@page_size = 5
+  @@page_size = 30
   def index
-  	@articles = ArticleCache.retrieve(30,0)
+  	@articles = ArticleCache.retrieve(@@page_size,0)
     @next_article = @@page_size
   end
 
   def load_articles
     # TODO: remember strong params
-    @next_article = params["next-article"] || 0
-    @articles = ArticleCache.retrieve(30, @next_article)
+    @next_article = (params["next_article"] || 0).to_i
+    @articles = ArticleCache.retrieve(@@page_size, @next_article)
     @next_article += @@page_size
-    render html: "articles"
+    render partial: "articles"
   end
 end
