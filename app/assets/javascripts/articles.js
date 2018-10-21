cbef7029870ec25cdcd820a8f2197efc48833ee0
Hemant Torsekar <hvt@me.com>
2018-10-21 19:45:21 -0400

    Load more posts works.

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 	var btn = document.getElementById('more-posts');
-	var ul = document.getElementById('recent-articles');
 
 	var next_article = document.getElementById('next-article');
-	
+	var button = document.getElementById("more-posts");
+
 	btn.addEventListener('click', function(event) {
 		Rails.ajax({
 			url: 'articles/load_articles',
@@ -12,8 +12,15 @@ document.addEventListener("DOMContentLoaded", function() {
 			data: "next_article=" + next_article.value,
 
 			success: function(document) {  
-				# ul.innerHTML = document.documentElement.innerHTML;
-				# alert(document.documentElement.innerHTML);
+
+				var newArticles = document.createElement("ul");
+				newArticles.className = "new-articles";
+				button.parentNode.removeChild(button);
+				var pN = next_article.parentNode;
+				newArticles.innerHTML = document.documentElement.innerHTML;
+
+				next_article.parentNode.removeChild(next_article);
+				pN.appendChild(newArticles, next_article);
 			}, 
 			error: function () {
 				console.log("error");
--- app/models/article_cache.rb
+++ app/models/article_cache.rb
@@ -5,25 +5,31 @@ class ArticleCache
 	attr_accessor :id, :title, :url
 	@@cache = nil
 
-	def initialize(id, title, url)
+	def initialize(id,title, url)
 		@id = id
 		@title = title
 		@url = url
 	end
 
-	def self.refresh
+	def self.refresh_index
 		response = HTTParty.get(article_index_url)
     article_ids = response.parsed_response
     @@cache = article_ids.map do |id| 
-    	article_url = article_base_url + "#{id}.json"
-    	article = HTTParty.get(article_url).parsed_response	
-    	self.new(id, article["title"], article["url"])
-    end	
+    	[id, nil]
+    end.to_h	
+	end
+
+	def self.fetch_article id 
+		return @@cache[id] if @@cache[id]
+		article_url = article_base_url + "#{id}.json"
+  	article = HTTParty.get(article_url).parsed_response	
+  	@@cache[id] = self.new(id, article["title"], article["url"])
 	end
 
 	def self.retrieve count=30, start_at_id=0
-		refresh unless @@cache
-		@@cache.slice(start_at_id, count)
+		refresh_index unless @@cache
+		article_ids = @@cache.keys.slice(start_at_id, count)
+		article_ids.map { |id| self.fetch_article id }
 	end
 
 private 
